4de2a872768221b5b2680594a5fb372bc5ffdc4d
rasithahamed50 <rasithahamed50@gmail.com>
2026-03-29 23:18:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,22 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    temperature = random.randint(25, 40)
-    humidity = random.randint(40, 80)
-
-    status = "Normal"
-    if temperature > 35:
-        status = "High Temperature ⚠️"
-
-    return render_template("index.html", temp=temperature, hum=humidity, status=status)
-
+    return render_template("index.html",
+                           temp=random.randint(25, 35),
+                           hum=random.randint(40, 80))
 
 @app.route('/data')
-def get_data():
+def data():
     return jsonify({
-        "temp": random.randint(25, 40)
+        "temp": random.randint(25, 35),
+        "hum": random.randint(40, 80)
     })
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
